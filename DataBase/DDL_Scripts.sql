602a5acb69b4a91ed2dfda91f4e64e78ba3a0ba4
EXTENSION <javiertuga@gmail.com>
2019-04-28 14:06:01 -0500

    dapper

--- DataBase/DDL_Scripts.sql
+++ DataBase/DDL_Scripts.sql
@@ -13,3 +13,58 @@ BEGIN
 	WHERE Name LIKE @filterByName
 
 END
+
+GO
+
+--CREATE OR ALTER
+ALTER PROCEDURE usp_InsertArtist
+@pName NVARCHAR(120)
+AS
+
+BEGIN
+	
+	IF NOT EXISTS(SELECT ArtistId FROM Artist WHERE Name = @pName)
+	BEGIN 
+		INSERT INTO Artist(Name)
+		VALUES (@pName)
+
+		SELECT SCOPE_IDENTITY()
+		
+	END
+	ELSE
+	BEGIN
+		SELECT 0
+	END
+END
+
+GO
+
+CREATE PROCEDURE usp_UpdateArtist
+@pName NVARCHAR(120),
+@pId INT
+AS
+
+BEGIN
+
+	IF NOT EXISTS(SELECT ArtistId FROM Artist 
+		WHERE Name = @pName)
+	BEGIN
+		UPDATE Artist SET Name = @pName
+		WHERE ArtistId = @pId
+	END
+
+END
+
+CREATE PROCEDURE usp_DeleteArtist
+@pId INT
+AS
+
+BEGIN
+
+	DELETE FROM Artist 
+	WHERE ArtistId = @pId
+
+
+END
+
+
